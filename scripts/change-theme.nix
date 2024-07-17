f92e7dc7c98baeb01690eb9ee7bb70f639a4aa00
Clément Baconnier <clement@baconnier.ch>
2024-07-17 23:48:54 +0200

    Attempt to fix dconf and xfconf update

--- scripts/change-theme.nix
+++ scripts/change-theme.nix
@@ -2,4 +2,6 @@
 
 pkgs.writeShellScriptBin "change-theme" ''
     nix run 'github:fore-stun/flakes/32f0498b9dc67e9e32e57ded04ba12718d2b125b#home-manager-specialisation'
+    ${pkgs.xfce.xfconf}/bin/xfconf-query --create --type string -c xsettings -p /Net/ThemeName -s "Papirus-Dark"
+    ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
   ''
