18e717652b02a14e9ebac3160260ed32deb5daa5
Clément Baconnier <clement@baconnier.ch>
2024-07-18 22:01:58 +0200

    Add greetd

--- hosts-config/system-packages.nix
+++ hosts-config/system-packages.nix
@@ -9,4 +9,15 @@
    git
    zsh
  ];
+ 
+services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        user = "greeter";
+      };
+    };
+  };
+
 }
