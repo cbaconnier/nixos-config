82b8d05f587c0da351c909a21c7304fca0e4bea1
Clément Baconnier <clement@baconnier.ch>
2024-07-23 18:43:50 +0200

    Remove plymouth

--- hosts/home/configuration.nix
+++ hosts/home/configuration.nix
@@ -33,11 +33,11 @@
   boot.initrd.enable = true;
   boot.initrd.systemd.enable = true;
 
-  boot.plymouth = {
-   enable = true;
-   theme = "circle_hud";
-   themePackages = [(pkgs.adi1090x-plymouth-themes.override {selected_themes = ["circle_hud"];})];
-  };
+ # boot.plymouth = {
+ #  enable = true;
+ #  theme = "circle_hud";
+ #  themePackages = [(pkgs.adi1090x-plymouth-themes.override {selected_themes = ["circle_hud"];})];
+ # };
 
   # Perform garbage collection weekly to maintain low disk usage
   nix.gc = {
