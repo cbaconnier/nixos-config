234cdaf5307b665027db1128ffc0ae8846cce1e4
Clément Baconnier <clement@baconnier.ch>
2024-07-29 23:21:13 +0200

    Add fonts

--- hosts-config/fonts.nix
+++ hosts-config/fonts.nix
@@ -6,6 +6,15 @@
    font-awesome
    powerline-fonts
    nerdfonts
+
+   noto-fonts
+   noto-fonts-cjk
+   noto-fonts-emoji
+   source-code-pro
+   source-han-mono
+   source-han-sans
+   source-han-serif
+   wqy_zenhei
   ];
  };
 }
