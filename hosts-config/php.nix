ab1a023b20d1502bbcc8744037e892370af72e9e
Clément Baconnier <clement@baconnier.ch>
2024-10-07 17:25:38 +0200

    Update PHP

--- hosts-config/php.nix
+++ hosts-config/php.nix
@@ -1,12 +1,15 @@
 { pkgs, ... }:
 
 # https://nixos.wiki/wiki/PHP
+# https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/php.section.md
 
 {
-
   environment.systemPackages = with pkgs; [
-    php
+    php83
     php83Packages.composer
   ];
 
+ environment.shellInit = ''
+    export PATH="$HOME/.config/composer/vendor/bin:$PATH"
+  '';
 }
