ee18b05215c1033b660648844d7bb043eed44887
Clément Baconnier <clement@baconnier.ch>
2025-01-16 20:59:23 +0100

    Add libreoffice

--- hosts-config/user-packages.nix
+++ hosts-config/user-packages.nix
@@ -1,93 +1,92 @@
-{ pkgs, inputs, ...}:
-{
+{ pkgs, inputs, ... }: {
   users.users.clement = {
-    packages = with pkgs; [ 
-     neofetch
-     tmux
-     zip
-     unzip
-     ripgrep # run with `rg`
-     jq
-     yq-go # run with `yq`: yaml processor
-     fzf
-     file
-     which
-     tree
-     vlc
-     btop
-     ethtool # query or control network driver and hardware settings
-     pciutils # PCI utilities: include `lspci`, `setpci`, `update-pciids` and `pcilmr` commands
-     usbutils # USB utilities: include `lsusb`, `usb-devices` and `usbhid-dump`
-     dnsutils # Network utilities: include `dig`, `nslookup` and `nsupdate`
-     firefox
-     pavucontrol
-     mangohud # A Vulkan and OpenGL overlay for monitoring FPS, temperatures, CPU/GPU load and more. https://wiki.archlinux.org/title/MangoHud
-     hyprpicker
-     xboxdrv # xbox controller driver
-     vesktop
-     lazygit
-     gh
-     yazi # ranger like
-     libnotify
-     clipse # TUI-based clipboard manager application
-     rsync
-     veracrypt
+    packages = with pkgs; [
+      neofetch
+      tmux
+      zip
+      unzip
+      ripgrep # run with `rg`
+      jq
+      yq-go # run with `yq`: yaml processor
+      fzf
+      file
+      which
+      tree
+      vlc
+      btop
+      ethtool # query or control network driver and hardware settings
+      pciutils # PCI utilities: include `lspci`, `setpci`, `update-pciids` and `pcilmr` commands
+      usbutils # USB utilities: include `lsusb`, `usb-devices` and `usbhid-dump`
+      dnsutils # Network utilities: include `dig`, `nslookup` and `nsupdate`
+      firefox
+      pavucontrol
+      mangohud # A Vulkan and OpenGL overlay for monitoring FPS, temperatures, CPU/GPU load and more. https://wiki.archlinux.org/title/MangoHud
+      hyprpicker
+      vesktop
+      lazygit
+      gh
+      yazi # ranger like
+      libnotify
+      clipse # TUI-based clipboard manager application
+      rsync
+      veracrypt
 
-     grim
-     slurp
+      grim
+      slurp
 
-     cbonsai # Grow bonsai trees in your terminal
+      cbonsai # Grow bonsai trees in your terminal
 
-     tigervnc
-     wayvnc
-     signal-desktop
-     obs-studio
-     spotify 
+      tigervnc
+      wayvnc
+      signal-desktop
+      obs-studio
+      spotify
 
-     gcc # C compiler
-     nodejs
+      libreoffice
 
-     cargo
-     rustc
-     
-     sqlite
-     mariadb-client
+      gcc # C compiler
+      nodejs
 
-     imv # Image viewer, also provide `imv-dir` that auto-selects the folder where the image is located, so that the next and previous commands function works in the same way as other image viewers.
+      cargo
+      rustc
 
-     ags # GTK widgets https://aylur.github.io/ags-docs/
-     libdbusmenu-gtk3 # Library for passing menu structures across DBus, Used by AGS for the try-system
-     home-manager  
+      sqlite
+      mariadb-client
 
-     lutris
-     wine
-     gamescope
-     protonup-qt
+      imv # Image viewer, also provide `imv-dir` that auto-selects the folder where the image is located, so that the next and previous commands function works in the same way as other image viewers.
 
-     fd # fast and user friendly alternative to `find`
+      ags # GTK widgets https://aylur.github.io/ags-docs/
+      libdbusmenu-gtk3 # Library for passing menu structures across DBus, Used by AGS for the try-system
+      home-manager
 
-     appimage-run
+      lutris
+      wine
+      gamescope
+      protonup-qt
 
-     inputs.swww.packages.${pkgs.system}.swww # Wallpaper https://github.com/LGFae/swww
-     
-     yt-dlp # Command-line tool to download videos from YouTube.com and other sites 
+      fd # fast and user friendly alternative to `find`
 
-     nix
+      appimage-run
+
+      inputs.swww.packages.${pkgs.system}.swww # Wallpaper https://github.com/LGFae/swww
+
+      yt-dlp # Command-line tool to download videos from YouTube.com and other sites
+
+      nix
     ];
   };
 
-# https://github.com/NixOS/nixpkgs/issues/306446#issuecomment-2081540768
- nixpkgs.overlays = [
-    (final: prev:
-    {
-      ags = prev.ags.overrideAttrs (old: {
-        buildInputs = old.buildInputs ++ [ pkgs.libdbusmenu-gtk3 ];
-      });
+  # https://github.com/NixOS/nixpkgs/issues/306446#issuecomment-2081540768
+  nixpkgs.overlays = [
+    (final: prev: {
+      ags = prev.ags.overrideAttrs
+        (old: { buildInputs = old.buildInputs ++ [ pkgs.libdbusmenu-gtk3 ]; });
     })
   ];
 
   services = {
-    gvfs.enable = true; # When installed, Thunar will show the trash can, removable media, and remote filesystems 
+    gvfs.enable =
+      true; # When installed, Thunar will show the trash can, removable media, and remote filesystems
     syncthing = {
       enable = true;
       openDefaultPorts = true;
