3f3dacaac09e6e6a5eaa368e853980c2feabe095
Clément Baconnier <clement@baconnier.ch>
2025-01-27 21:11:28 +0100

    Add ente-photos

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,26 +1,26 @@
-{ pkgs, ... }:
-{
- imports = [
-  ./ags
-  ./clipse
-  ./easyeffects
-  ./flameshot
-  ./git
-  ./gtk
-  ./hyprland
-  ./kitty
-  ./nvim
-  ./pipewire
-  ./rofi
-  ./tmux
-  ./vesktop
-  ./wallpapers
-  ./zsh
- ];
+{ pkgs, ... }: {
+  imports = [
+    ./ags
+    ./clipse
+    ./easyeffects
+    ./flameshot
+    ./git
+    ./gtk
+    ./hyprland
+    ./kitty
+    ./nvim
+    ./pipewire
+    ./rofi
+    ./tmux
+    ./vesktop
+    ./wallpapers
+    ./zsh
+  ];
 
- home.packages = with pkgs; [
-  geforcenow-electron
-  flameshot-unstable
- ];
+  home.packages = with pkgs; [
+    ente-photos
+    geforcenow-electron
+    flameshot-unstable
+  ];
 
 }
