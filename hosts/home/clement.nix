804d33ff952a23e882ac8fa35ceffd80973a2b14
Clément Baconnier <clement@baconnier.ch>
2025-01-30 19:58:16 +0100

    Disable WLR_NO_HARDWARE_CURSORS

--- hosts/home/clement.nix
+++ hosts/home/clement.nix
@@ -1,42 +1,40 @@
-{ inputs, outputs, pkgs, ...}:
+{ inputs, outputs, pkgs, ... }:
 
 {
- home.username = "clement";
- home.homeDirectory = "/home/clement";
+  home.username = "clement";
+  home.homeDirectory = "/home/clement";
 
- imports = [
-  ./../../home-manager
- ];
+  imports = [ ./../../home-manager ];
 
-
- home.sessionVariables = {
+  home.sessionVariables = {
     EDITOR = "nvim";
     BROWSER = "firefox";
     TERMINAL = "kitty";
 
-
     #I'm experimenting thoses. I haven't encountered any issue without them though
-    GBM_BACKEND= "nvidia-drm";           # Use NVIDIA DRM for buffer management
-    __GLX_VENDOR_LIBRARY_NAME= "nvidia"; # Force the use of NVIDIA's GLX library
-    LIBVA_DRIVER_NAME= "nvidia";         # Use NVIDIA for hardware acceleration
-    __GL_VRR_ALLOWED="1";                # Enable Variable Refresh Rate if available
-    WLR_NO_HARDWARE_CURSORS = "1";       # Disable hardware cursors 
-    WLR_RENDERER_ALLOW_SOFTWARE = "1";   # Allow software rendering as fallback
-    CLUTTER_BACKEND = "wayland";         # Use Wayland backend for Clutter
-    WLR_RENDERER = "gles2";              # Use OpenGL as the renderer for wlroots (`vulkan` is not supported by Hyprland)
+    GBM_BACKEND = "nvidia-drm"; # Use NVIDIA DRM for buffer management
+    __GLX_VENDOR_LIBRARY_NAME =
+      "nvidia"; # Force the use of NVIDIA's GLX library
+    LIBVA_DRIVER_NAME = "nvidia"; # Use NVIDIA for hardware acceleration
+    __GL_VRR_ALLOWED = "1"; # Enable Variable Refresh Rate if available
+    # WLR_NO_HARDWARE_CURSORS = "1";       # Disable hardware cursors 
+    WLR_RENDERER_ALLOW_SOFTWARE = "1"; # Allow software rendering as fallback
+    CLUTTER_BACKEND = "wayland"; # Use Wayland backend for Clutter
+    WLR_RENDERER =
+      "gles2"; # Use OpenGL as the renderer for wlroots (`vulkan` is not supported by Hyprland)
 
-    XDG_CURRENT_DESKTOP = "Hyprland";    # Set the current desktop environment
-    XDG_SESSION_DESKTOP = "Hyprland";    # Set the desktop session
-    XDG_SESSION_TYPE = "wayland";        # Specify the session type as Wayland
- };
+    XDG_CURRENT_DESKTOP = "Hyprland"; # Set the current desktop environment
+    XDG_SESSION_DESKTOP = "Hyprland"; # Set the desktop session
+    XDG_SESSION_TYPE = "wayland"; # Specify the session type as Wayland
+  };
 
- # Enable the overlays.additions to access our custom pkgs in home-manager
- nixpkgs.overlays = [ outputs.overlays.additions  ];
- 
- # Allow unfree packages
- nixpkgs.config.allowUnfree = true;
+  # Enable the overlays.additions to access our custom pkgs in home-manager
+  nixpkgs.overlays = [ outputs.overlays.additions ];
 
- home.stateVersion = "24.11";
- programs.home-manager.enable = true;
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  home.stateVersion = "24.11";
+  programs.home-manager.enable = true;
 
 }
