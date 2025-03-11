a245694704d82498a9310bdd87c7e0270744fdcd
Clément Baconnier <clement@baconnier.ch>
2025-03-11 20:28:18 +0100

    Upgrade hyprland to v46.2

--- hosts-config/hyprland.nix
+++ hosts-config/hyprland.nix
@@ -1,6 +1,5 @@
-{ pkgs, inputs, ...}: 
-{
-  
+{ pkgs, inputs, ... }: {
+
   # This file enable the development version when stable one get unstable
   # You first need to uncomment the file in configuration.nix
   # You also need to disable (`enable=false`) hyprland in home-manager
@@ -9,15 +8,16 @@
 
   # https://wiki.hyprland.org/Nix/Cachix/
   nix.settings = {
-   substituters = ["https://hyprland.cachix.org"];
-   trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="]; 
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys =
+      [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
 
   programs.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland; # xdphls
-  	xwayland.enable = true;
+    # package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland; # xdphls
+    xwayland.enable = true;
   };
 
   # Hint electron apps to use wayland:
