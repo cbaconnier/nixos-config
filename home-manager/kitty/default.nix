0a317affb0314ddbc651849ef7898806dd87b2f8
Clément Baconnier <clement@baconnier.ch>
2025-06-06 12:28:47 +0200

    Update kitty config

--- home-manager/kitty/default.nix
+++ home-manager/kitty/default.nix
@@ -1,28 +1,30 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
 
 {
- programs.kitty = {
-  enable = true;
-  settings = {
-   font_family = "JetBrains Mono Regular";
-   font_size = 13;
-   background_opacity = "0.95";
+  programs.kitty = {
+    enable = true;
+    settings = {
+      font_family = "JetBrains Mono Regular";
+      font_size = 13;
+      background_opacity = "0.95";
+      cursor_trail = 1;
+      cursor_trail_decay = "0.1 0.4";
+    };
+    keybindings = { "kitty_mod+t" = "new_tab_with_cwd"; };
   };
- };
 
-specialisation.light.configuration = {
-  programs.kitty.theme = "Catppuccin-Latte";
-};
+  specialisation.light.configuration = {
+    programs.kitty.theme = "Catppuccin-Latte";
+  };
 
-specialisation.dark.configuration = {
-  programs.kitty.theme = "Catppuccin-Mocha";
-};
+  specialisation.dark.configuration = {
+    programs.kitty.theme = "Catppuccin-Mocha";
+  };
 
-
- home.file = {
-#  ".config/kitty/themes/macchiato.conf".source = ./macchiato.conf;
-  ".config/kitty/themes/mocha.conf".source = ./mocha.conf;
-  ".config/kitty/themes/latte.conf".source = ./latte.conf;
- };
+  home.file = {
+    #  ".config/kitty/themes/macchiato.conf".source = ./macchiato.conf;
+    ".config/kitty/themes/mocha.conf".source = ./mocha.conf;
+    ".config/kitty/themes/latte.conf".source = ./latte.conf;
+  };
 
 }
