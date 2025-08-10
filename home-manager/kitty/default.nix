bbd2a4abcd3259584582d7356b1c14649ce88f42
Clément Baconnier <clement@baconnier.ch>
2025-08-10 09:46:42 +0200

    Update kitty keybindings

--- home-manager/kitty/default.nix
+++ home-manager/kitty/default.nix
@@ -9,8 +9,15 @@
       background_opacity = "0.95";
       cursor_trail = 1;
       cursor_trail_decay = "0.1 0.4";
+
     };
-    keybindings = { "kitty_mod+t" = "new_tab_with_cwd"; };
+    keybindings = {
+      "kitty_mod+t" = "new_tab_with_cwd";
+      "kitty_mod+enter" = "new_window_with_cwd";
+      "kitty_mod+f12" = "increase_font_size";
+      "kitty_mod+f11" = "decrease_font_size";
+      "kitty_mod+f10" = "reset_font_size";
+    };
   };
 
   specialisation.light.configuration = {
