ff50b03b73be9a286c6a374de2fd3c8614f49904
Clément Baconnier <clement@baconnier.ch>
2025-09-15 09:47:07 +0200

    Run nix flake update and fix rofi

--- home-manager/rofi/default.nix
+++ home-manager/rofi/default.nix
@@ -1,40 +1,41 @@
-{ pkgs, lib, config, ...}:
+{ pkgs, lib, config, ... }:
 
 {
- programs.rofi = {
-  enable = true;
-  package = pkgs.rofi-wayland;
-  plugins = [ pkgs.rofi-calc ];
- 
-  font = "JetBrains Mono Regular 13";
-  location = "center";
-  terminal = "kitty";
+  programs.rofi = {
+    enable = true;
+    plugins = [ pkgs.rofi-calc ];
 
-  extraConfig = {
-   modi = "drun";
-   icon-theme = "Numix-Circle";
-   show-icons = true;
-   disable-history = false;
-   drun-display-format = "{icon} {name}";
-   hide-scrollbar = true;
-   display-drun = "   Apps ";
-   sidebar-mode = true;
-   border-radius = 10;
+    font = "JetBrains Mono Regular 13";
+    location = "center";
+    terminal = "kitty";
+
+    extraConfig = {
+      modi = "drun";
+      icon-theme = "Numix-Circle";
+      show-icons = true;
+      disable-history = false;
+      drun-display-format = "{icon} {name}";
+      hide-scrollbar = true;
+      display-drun = "   Apps ";
+      sidebar-mode = true;
+      border-radius = 10;
+    };
+
   };
 
- };
+  specialisation.light.configuration = {
+    programs.rofi.theme = "~/.config/rofi/themes/catppuccin-latte.rasi";
+  };
 
-specialisation.light.configuration = {
-  programs.rofi.theme = "~/.config/rofi/themes/catppuccin-latte.rasi";
-};
+  specialisation.dark.configuration = {
+    programs.rofi.theme = "~/.config/rofi/themes/catppuccin-macchiato.rasi";
+  };
 
-specialisation.dark.configuration = {
-  programs.rofi.theme = "~/.config/rofi/themes/catppuccin-macchiato.rasi";
-};
-
- home.file = {
-  ".config/rofi/themes/catppuccin-macchiato.rasi".source = ./catppuccin-macchiato.rasi;
-  ".config/rofi/themes/catppuccin-latte.rasi".source = ./catppuccin-latte.rasi;
- };
+  home.file = {
+    ".config/rofi/themes/catppuccin-macchiato.rasi".source =
+      ./catppuccin-macchiato.rasi;
+    ".config/rofi/themes/catppuccin-latte.rasi".source =
+      ./catppuccin-latte.rasi;
+  };
 
 }
