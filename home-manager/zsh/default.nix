6f9dc9067a5261761c5d999f06e9dadf3a5fdacb
Clément Baconnier <clement@baconnier.ch>
2025-09-19 10:07:16 +0200

    Add sail-bootstrap alias to zsh

--- home-manager/zsh/default.nix
+++ home-manager/zsh/default.nix
@@ -1,29 +1,29 @@
 { pkgs, ... }:
 
 {
- programs.zsh = {
-   enable = true;
-   syntaxHighlighting.enable = true;
-   shellAliases = {
-      sail = "sh $([ -f sail ] && echo sail || echo vendor/bin/sail)";
-   };
-   oh-my-zsh = {
+  programs.zsh = {
     enable = true;
-    theme = "refined";
-    plugins = [ 
-     "git" 
-     "docker"
-     "colored-man-pages" 
-     "emoji"
-     "emotty" 
-    ]; 
-   };
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      sail = "sh $([ -f sail ] && echo sail || echo vendor/bin/sail)";
+      sail-bootstrap = ''
+        docker run --rm \
+          -u "$(id -u):$(id -g)" \
+          -v "$(pwd):/var/www/html" \
+          -w /var/www/html \
+          laravelsail/php83-composer:latest \
+          composer install --ignore-platform-reqs
+      '';
+    };
+    oh-my-zsh = {
+      enable = true;
+      theme = "refined";
+      plugins = [ "git" "docker" "colored-man-pages" "emoji" "emotty" ];
+    };
 
-   zplug = {
-     enable = true;
-     plugins = [
-       { name = "jessarcher/zsh-artisan"; }
-     ];
-   };
- };
+    zplug = {
+      enable = true;
+      plugins = [{ name = "jessarcher/zsh-artisan"; }];
+    };
+  };
 }
