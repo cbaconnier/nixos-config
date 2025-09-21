a19279ff6848cfa87c487379cc4d7a88676f0142
Clément Baconnier <clement@baconnier.ch>
2025-09-21 10:24:10 +0200

    Remove astal3

--- hosts-config/user-packages.nix
+++ hosts-config/user-packages.nix
@@ -65,7 +65,6 @@
       imv # Image viewer, also provide `imv-dir` that auto-selects the folder where the image is located, so that the next and previous commands function works in the same way as other image viewers.
 
       #ags # GTK widgets https://aylur.github.io/ags-docs/
-      inputs.astal.packages.${pkgs.system}.astal3
       inputs.ags.packages.${pkgs.system}.agsFull
       libdbusmenu-gtk3 # Library for passing menu structures across DBus, Used by AGS for the system tray
       home-manager
