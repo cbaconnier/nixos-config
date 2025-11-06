2a230cf8acd643cdd28e065537d7fff433a254cc
Clément Baconnier <clement@baconnier.ch>
2025-11-06 19:25:18 +0100

    Fix syntax

--- hosts/laptop/hardware-configuration.nix
+++ hosts/laptop/hardware-configuration.nix
@@ -4,31 +4,30 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules =
+    [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/4b7e06e2-7ff3-44e3-8767-4c1c1d198726";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/4b7e06e2-7ff3-44e3-8767-4c1c1d198726";
+    fsType = "ext4";
+  };
 
-  boot.initrd.luks.devices."luks-e66caa6a-957e-4d78-8e53-48b749979ed0".device = "/dev/disk/by-uuid/e66caa6a-957e-4d78-8e53-48b749979ed0";
+  boot.initrd.luks.devices."luks-e66caa6a-957e-4d78-8e53-48b749979ed0".device =
+    "/dev/disk/by-uuid/e66caa6a-957e-4d78-8e53-48b749979ed0";
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/9501-FA9C";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/9501-FA9C";
+    fsType = "vfat";
+    options = [ "fmask=0077" "dmask=0077" ];
+  };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/34276b32-2058-46d3-a072-6577eef827bf"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/34276b32-2058-46d3-a072-6577eef827bf"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -39,5 +38,6 @@
   # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
