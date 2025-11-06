fe99405be9af59febb05c434f335f9047a52de22
Clément Baconnier <clement@baconnier.ch>
2025-11-06 15:01:57 +0100

    Fix wallpaper

--- home-manager/wallpapers/default.nix
+++ home-manager/wallpapers/default.nix
@@ -8,20 +8,20 @@ in {
   # todo: Add default wallpapers
 
   # specialisation.dark.configuration = {
-  #   home.activation.set-wallpaper =
-  #     config.lib.dag.entryAfter [ "writeBoundary" ] ''
-  #       ${pkgs.bash}/bin/bash -c '
-  #          ${set_wallpaper_script}/bin/set-wallpaper "$(${pkgs.findutils}/bin/find ${wallpapers_dir} -name "dark.*" | ${pkgs.coreutils}/bin/head -n 1)"
-  #       '
-  #     '';
-  # };
-
-  # specialisation.light.configuration = {
   home.activation.set-wallpaper =
     config.lib.dag.entryAfter [ "writeBoundary" ] ''
       ${pkgs.bash}/bin/bash -c '
-         ${set_wallpaper_script}/bin/set-wallpaper "$(${pkgs.findutils}/bin/find ${wallpapers_dir} -name "light.*" | ${pkgs.coreutils}/bin/head -n 1)"
+         ${set_wallpaper_script}/bin/set-wallpaper "$(${pkgs.findutils}/bin/find ${wallpapers_dir} -name "dark.*" | ${pkgs.coreutils}/bin/head -n 1)"
       '
     '';
+  # };
+
+  # specialisation.light.configuration = {
+  # home.activation.set-wallpaper =
+  #   config.lib.dag.entryAfter [ "writeBoundary" ] ''
+  #     ${pkgs.bash}/bin/bash -c '
+  #        ${set_wallpaper_script}/bin/set-wallpaper "$(${pkgs.findutils}/bin/find ${wallpapers_dir} -name "light.*" | ${pkgs.coreutils}/bin/head -n 1)"
+  #     '
+  #   '';
   # };
 }
