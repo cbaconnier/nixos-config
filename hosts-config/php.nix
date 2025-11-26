c2cc0cf873333295e2512b1d3c48ae7afc7495f7
Clément Baconnier <clement@baconnier.ch>
2025-11-26 13:19:54 +0100

    Use php84

--- hosts-config/php.nix
+++ hosts-config/php.nix
@@ -4,12 +4,9 @@
 # https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/php.section.md
 
 {
-  environment.systemPackages = with pkgs; [
-    php83
-    php83Packages.composer
-  ];
+  environment.systemPackages = with pkgs; [ php84 php84Packages.composer ];
 
- environment.shellInit = ''
+  environment.shellInit = ''
     export PATH="$HOME/.config/composer/vendor/bin:$PATH"
   '';
 }
