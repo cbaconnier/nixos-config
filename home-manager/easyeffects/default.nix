f112b6bbcc3b2c1bd3b5ecdca7d37df33544331f
Clément Baconnier <clement@baconnier.ch>
2026-05-30 12:31:16 +0200

    Add EasyEffects output presets and per-device autoload rules

--- home-manager/easyeffects/default.nix
+++ home-manager/easyeffects/default.nix
@@ -1,4 +1,4 @@
-{ pkgs,... }:
+{ ... }:
 
 # https://home-manager-options.extranix.com/?query=easyeffects&release=master
 # Require dconf to be enabled
@@ -6,13 +6,27 @@
 {
 
   services.easyeffects = {
-   enable = true;
-   preset = "masc_voice_noise_reduction";
+    enable = true;
   };
 
   home.file = {
-   ".config/easyeffects/input/masc_voice_noise_reduction.json".source = ./masc_voice_noise_reduction.json;
-   ".local/share/easyeffects/input/masc_voice_noise_reduction.json".source = ./masc_voice_noise_reduction.json;
+    ".local/share/easyeffects/input/masc_voice_noise_reduction.json".source =
+      ./input-presets/masc_voice_noise_reduction.json;
+    ".local/share/easyeffects/output/thinkpad_z13_gen1_output.json".source =
+      ./output-presets/thinkpad_z13_gen1_output.json;
+    ".local/share/easyeffects/output/blank_output.json".source = ./output-presets/blank_output.json;
+    ".local/share/easyeffects/output/Beyerdynamic DT 770 Pro (80 Ohm).json".source =
+      ./output-presets + "/Beyerdynamic DT 770 Pro (80 Ohm).json";
+
+    ".local/share/easyeffects/autoload/output/alsa_output.pci-0000_c7_00.6.HiFi__Speaker__sink:Speaker.json".source =
+      ./output-autoload/laptop.json;
+    ".local/share/easyeffects/autoload/output/alsa_output.usb-Creative_Technology_Ltd_Sound_Blaster_Play__3_00203568-00.analog-stereo:Speakers.json".source =
+      ./output-autoload/edifier-laptop.json;
+    ".local/share/easyeffects/autoload/output/alsa_output.pci-0000_c7_00.6.HiFi__Headphones__sink:Headphones.json".source =
+      ./output-autoload/dt770pro-laptop.json;
+
+    ".local/share/easyeffects/autoload/input/alsa_input.usb-C-Media_Electronics_Inc._USB_Advanced_Audio_Device-00.analog-stereo:Microphone.json".source =
+      ./input-autoload/rode-laptop.json;
   };
 
 }
