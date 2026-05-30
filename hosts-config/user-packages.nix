1176082b43a061fea484186c9773cdd6795a94ff
Clément Baconnier <clement@baconnier.ch>
2026-05-30 13:16:51 +0200

    Run nix flake update and fix lutris

--- hosts-config/user-packages.nix
+++ hosts-config/user-packages.nix
@@ -95,7 +95,31 @@
       libdbusmenu-gtk3 # Library for passing menu structures across DBus, Used by AGS for the system tray
       home-manager
 
-      lutris
+      # lutris
+      # TODO: remove override when openldap is fixed
+      (lutris.override {
+        # Intercept buildFHSEnv to modify target packages
+        buildFHSEnv =
+          args:
+          pkgs.buildFHSEnv (
+            args
+            // {
+              multiPkgs =
+                envPkgs:
+                let
+                  # Fetch original package list
+                  originalPkgs = args.multiPkgs envPkgs;
+
+                  # Disable tests for openldap
+                  customLdap = envPkgs.openldap.overrideAttrs (_: {
+                    doCheck = false;
+                  });
+                in
+                # Replace broken openldap with the custom one
+                builtins.filter (p: (p.pname or "") != "openldap") originalPkgs ++ [ customLdap ];
+            }
+          );
+      })
       wine
       mono
       gamescope
