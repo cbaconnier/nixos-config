5fb8cae4579a032bf5878c62b35a48fb8797aaff
Clément Baconnier <clement@baconnier.ch>
2026-07-09 18:10:12 +0200

    Set up home audio device picker

--- home-manager/ags/home.nix
+++ home-manager/ags/home.nix
@@ -3,6 +3,27 @@
 {
   imports = [ ./. ];
 
-  # node.name via: wpctl inspect <id> | grep node.name
-  # programs.agsAudio.speakers.rename = { "alsa_output...." = "Casque"; };
+  # node.name via: wpctl list, wpctl inspect <id> | grep node.name
+  programs.agsAudio = {
+    speakers = {
+      ignore = [
+        "alsa_output.pci-0000_01_00.1.hdmi-stereo"
+        "alsa_output.usb-C-Media_Electronics_Inc._USB_Advanced_Audio_Device-00.analog-stereo" # Rode NT OUT
+        "easyeffects_sink"
+      ];
+      rename = {
+        "alsa_output.pci-0000_00_1f.3.analog-stereo" = "Edifier | Headphones";
+      };
+    };
+    microphones = {
+      ignore = [
+        "easyeffects_source"
+      ];
+      rename = {
+        "alsa_input.usb-C-Media_Electronics_Inc._USB_Advanced_Audio_Device-00.analog-stereo" =
+          "Rode NT USB";
+        "alsa_input.pci-0000_00_1f.3.analog-stereo" = "Headset";
+      };
+    };
+  };
 }
--- home-manager/ags/laptop.nix
+++ home-manager/ags/laptop.nix
@@ -3,6 +3,7 @@
 {
   imports = [ ./. ];
 
+  # node.name via: wpctl list, wpctl inspect <id> | grep node.name
   programs.agsAudio = {
     speakers = {
       ignore = [
@@ -11,6 +12,7 @@
         "alsa_output.pci-0000_c7_00.1.HiFi__HDMI3__sink"
         "alsa_output.pci-0000_c7_00.1.HiFi__HDMI4__sink"
         "alsa_output.usb-C-Media_Electronics_Inc._USB_Advanced_Audio_Device-00.analog-stereo" # Rode NT OUT
+        "easyeffects_sink"
       ];
       rename = {
         "alsa_output.pci-0000_c7_00.6.HiFi__Speaker__sink" = "Speakers";
@@ -25,12 +27,13 @@
       ignore = [
         "alsa_input.usb-Lenovo_ThinkPad_Thunderbolt_4_Dock_USB_Audio_000000000000-00.mono-fallback"
         "alsa_input.usb-Creative_Technology_Ltd_Sound_Blaster_Play__3_00203568-00.analog-stereo"
+        "easyeffects_source"
       ];
       rename = {
         "alsa_input.usb-C-Media_Electronics_Inc._USB_Advanced_Audio_Device-00.analog-stereo" =
           "Rode NT USB";
-        "alsa_input.pci-0000_c7_00.6.HiFi__Mic1__source" = "Laptop intégré";
-        "alsa_input.pci-0000_c7_00.6.HiFi__Mic2__source" = "Laptop jack";
+        "alsa_input.pci-0000_c7_00.6.HiFi__Mic1__source" = "Built-in";
+        "alsa_input.pci-0000_c7_00.6.HiFi__Mic2__source" = "Headset";
       };
     };
   };
