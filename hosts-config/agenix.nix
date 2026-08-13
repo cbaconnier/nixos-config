1803f1139c89ea058d73b3e509073ea43fa89604
Clément Baconnier <clement@baconnier.ch>
2026-08-13 09:10:19 +0200

    Fix deprecation notice

--- hosts-config/agenix.nix
+++ hosts-config/agenix.nix
@@ -1,6 +1,6 @@
 { inputs, pkgs, ... }:
 {
   imports = [ inputs.agenix.nixosModules.default ];
-  environment.systemPackages = [ inputs.agenix.packages.${pkgs.system}.default ];
+  environment.systemPackages = [ inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default ];
   age.identityPaths = [ "/home/clement/.ssh/id_ed25519_nixos" ];
 }
