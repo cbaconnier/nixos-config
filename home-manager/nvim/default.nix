89eaed02f2df8146081f4e6e6eb4590d29c44309
Clément Baconnier <clement@baconnier.ch>
2026-08-20 07:42:42 +0200

    Update phpantom to v0.10.0

--- home-manager/nvim/default.nix
+++ home-manager/nvim/default.nix
@@ -7,11 +7,11 @@
 let
   phpantom_lsp = pkgs.stdenv.mkDerivation rec {
     pname = "phpantom_lsp";
-    version = "0.9.0";
+    version = "0.10.0";
 
     src = pkgs.fetchurl {
       url = "https://github.com/AJenbo/phpantom_lsp/releases/download/${version}/phpantom_lsp-x86_64-unknown-linux-gnu.tar.gz";
-      hash = "sha256:cede0af180ab4d6980194659e76f7b211071dcef40c54f2506e3cc7984c633f9";
+      hash = "sha256-KzhViHec39uAQ3H5nJFhsO9BtuV9NO1OG2ljaG+B3CA=";
     };
 
     nativeBuildInputs = [ pkgs.autoPatchelfHook ];
