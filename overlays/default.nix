5f2ba39281e5723b56efa4764d876776070720bc
Clément Baconnier <clement@baconnier.ch>
2026-09-02 16:13:38 +0200

    Update freetube to 0.25.3

--- overlays/default.nix
+++ overlays/default.nix
@@ -13,16 +13,16 @@
     # });
 
     freetube = prev.freetube.overrideAttrs (old: rec {
-      version = "0.25.2";
+      version = "0.25.3";
       src = final.fetchFromGitHub {
         owner = "FreeTubeApp";
         repo = "FreeTube";
         tag = "v${version}-beta";
-        hash = "sha256-A25I64GP4FRyP21W5QuVvrWpThyU7hDosO25vkIx0UY=";
+        hash = "sha256-eaf10W8dBMJDlqOFd57hsqQqRcBOYAMavLrfmkuRbSw=";
       };
       pnpmDeps = old.pnpmDeps.override {
         inherit version src;
-        hash = "sha256-1OnmJi4xCxMALAac4jnLOKg5N/t3pcHgM0AgvF1+DpM=";
+        hash = "sha256-rsgDxK6X2EzgPwIb9A9I+STkKI882i8jDuL4pO5kJHU=";
       };
     });
 
