78af7734709dacd24fb4dfc21ac076d4ed6aaa38
Clément Baconnier <clement@baconnier.ch>
2026-09-02 16:40:08 +0200

    Remove duplicate comment

--- hosts-config/glib.nix
+++ hosts-config/glib.nix
@@ -11,9 +11,6 @@
     GSETTINGS_SCHEMA_DIR =
       "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas";
 
-    # XCURSOR_PATH = lib.mkForce
-    #   "/run/current-system/sw/share/icons:$HOME/.icons:$HOME/.local/share/icons";
-
     XCURSOR_PATH = lib.mkForce
       "/run/current-system/sw/share/icons:$HOME/.icons:$HOME/.local/share/icons";
 
