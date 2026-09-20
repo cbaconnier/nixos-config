f74abe3dc365c1aecae089a51c8bf1151ac72140
Clément Baconnier <clement@baconnier.ch>
2026-09-20 10:09:28 +0200

    Fix lutris

--- hosts-config/user-packages.nix
+++ hosts-config/user-packages.nix
@@ -99,31 +99,7 @@
 
       home-manager
 
-      # lutris
-      # TODO: remove override when openldap is fixed
-      (lutris.override {
-        # Intercept buildFHSEnv to modify target packages
-        buildFHSEnv =
-          args:
-          pkgs.buildFHSEnv (
-            args
-            // {
-              multiPkgs =
-                envPkgs:
-                let
-                  # Fetch original package list
-                  originalPkgs = args.multiPkgs envPkgs;
-
-                  # Disable tests for openldap
-                  customLdap = envPkgs.openldap.overrideAttrs (_: {
-                    doCheck = false;
-                  });
-                in
-                # Replace broken openldap with the custom one
-                builtins.filter (p: (p.pname or "") != "openldap") originalPkgs ++ [ customLdap ];
-            }
-          );
-      })
+      lutris
       wine
       mono
       gamescope
